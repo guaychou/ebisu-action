7de24c25247690bca0e52842b4e13fead2a0b52e
kevin.harnata <kevin.harnata@gdn-commerce.com>
2021-10-26 13:27:08 +0700

    fixing axios

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const axios = require('axios');
-
+const validUrl = require('valid-url');
 try {
   // Get service name input
   const service = core.getInput('service-name');
   const message = core.getInput('message');
-  const address = core.getInput('address')
-  const time = (new Date()).toTimeString();
-  var validUrl = require('valid-url');
-
+  const address = core.getInput('address') + "/api/v1/alertwithmessage";
   validUrl.isUri(address)
-  core.setOutput("time", time);
-  address = address + "/api/v1/alertwithmessage"
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify({
       service : service,
